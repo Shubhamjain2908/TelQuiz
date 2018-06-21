fb052c9a6ae844969ef0c1769b119d820c2c9793
shubham jain <shubhamjain2908@gmail.com>
2018-06-21 18:44:56 +0530

    initial commit

---
+++ src/main/java/com/telusko/quiz/TeluskoQuizApplication.java
@@ -0,0 +1,12 @@
+package com.telusko.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluskoQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluskoQuizApplication.class, args);
+	}
+}
